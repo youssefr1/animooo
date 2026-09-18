6d54475608b197f0c7ad36fee17907fa3f571b4d
Youssefr1 <youssframa09@gmail.com>
2026-09-18 11:35:01 +0300

    otp verification screen refactor

--- lib/core/widgets/custom_text_form_feild.dart
+++ lib/core/widgets/custom_text_form_feild.dart
@@ -3,15 +3,19 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../resources/color_manger.dart';
 
-class Custom_text_form_feild extends StatelessWidget {
-  const Custom_text_form_feild({
+class CustomTextFormFeild extends StatelessWidget {
+  const CustomTextFormFeild({
     super.key,
     this.hintText,
     this.suffixIcon,
     this.validator,
     this.obscureText = false,
     required this.keyboardType,
-    this.fillColor, required this.controller,
+    this.fillColor,
+    this.controller,
+    this.textAlign,
+    this.focusNode,
+    this.borderSide, this.onChanged,
   });
 
   final String? hintText;
@@ -21,15 +25,26 @@ class Custom_text_form_feild extends StatelessWidget {
 
   final TextInputType keyboardType;
   final Color? fillColor;
-  final TextEditingController controller ;
+  final TextEditingController? controller;
+
+  final TextAlign? textAlign;
+  final FocusNode? focusNode;
+  final BorderSide? borderSide;
+  final ValueChanged<String>? onChanged;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 44.h,
       child: TextFormField(
+        onChanged: onChanged,
+        focusNode: focusNode,
+        textAlign: textAlign ?? TextAlign.start,
         controller: controller,
-        style: TextStyle(fontSize: 16.sp, color: ColorManger.primary),
+        style: TextStyle(
+          fontSize: 16.sp,
+          color: ColorManger.primary,
+        ),
         textAlignVertical: TextAlignVertical.center,
         obscureText: obscureText,
         obscuringCharacter: '*',
@@ -39,7 +54,10 @@ class Custom_text_form_feild extends StatelessWidget {
         validator: validator,
         keyboardType: keyboardType,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 16),
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 12.w,
+            vertical: 16,
+          ),
           suffixIcon: suffixIcon,
           filled: true,
           fillColor: fillColor ?? const Color(0xFFF6F6F6),
@@ -55,17 +73,23 @@ class Custom_text_form_feild extends StatelessWidget {
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10.r),
-            borderSide: BorderSide.none,
+            borderSide: borderSide ?? BorderSide.none,
           ),
 
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10.r),
-            borderSide: BorderSide(width: 3, color: ColorManger.primary),
+            borderSide: BorderSide(
+              width: 3,
+              color: ColorManger.primary,
+            ),
           ),
 
           errorBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10.r),
-            borderSide: BorderSide(width: 3, color: Colors.red),
+            borderSide: BorderSide(
+              width: 3,
+              color: Colors.red,
+            ),
           ),
         ),
       ),
